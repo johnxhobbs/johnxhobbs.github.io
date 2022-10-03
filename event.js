c9611d32755d9f1b8db3dbc15aa37d330963a4eb
johnhobbs <johnxhobbs@gmail.com>
2022-10-03 16:53:32 +0100

    Minor updates - need to test touch

--- event.js
+++ event.js
@@ -1,13 +1,12 @@
 
 // Pan behaviour: go right up to edge
 $('body').mousemove(function(e){
-    var mousePosX = 50 + (e.pageX/$(window).width())*25;
-    var mousePosY = 50 + (e.pageY/$(window).height())*25;
+    var mousePosX = 0 + (e.pageX/$(window).width())*100;
+    var mousePosY = 0 + (e.pageY/$(window).height())*100;
     $('body').css('background-position', mousePosX + '% ' +  mousePosY + '%');
 });
 
 // Set initial zoom to be zoom out
-// TODO: make zoom in focus on mouse
 var zoomedIn = false
 $('body').click(function(e){
     zoomedIn = !zoomedIn
