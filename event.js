0a77cbc2614e937a2b05d769483320ec37dd1832
johnhobbs <johnxhobbs@gmail.com>
2022-10-03 17:12:03 +0100

    Added 10% scroll boundary

--- event.js
+++ event.js
@@ -1,8 +1,13 @@
 
+const limitNumberWithinRange = (num, min = 0, max = 100) => {
+    const parsed = parseInt(num);
+    return Math.min(Math.max(parsed, min), max);
+  };
+
 // Pan behaviour: go right up to edge
 $('body').mousemove(function(e){
-    var mousePosX = 0 + (e.pageX/$(window).width())*100;
-    var mousePosY = 0 + (e.pageY/$(window).height())*100;
+    var mousePosX = limitNumberWithinRange(0 + (e.pageX/$(window).width())*120 - 10);
+    var mousePosY = limitNumberWithinRange(0 + (e.pageY/$(window).height())*120 - 10);
     $('body').css('background-position', mousePosX + '% ' +  mousePosY + '%');
 });
 
