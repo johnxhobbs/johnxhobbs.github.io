08fc2af0b2ddc786a8c46399e680a5f6df4e33f2
johnhobbs <johnxhobbs@gmail.com>
2022-10-03 11:57:20 +0100

    Click zoom working

--- event.js
+++ event.js
@@ -1,5 +1,16 @@
+
+// Pan behaviour: go right up to edge
 $('body').mousemove(function(e){
     var mousePosX = 50 + (e.pageX/$(window).width())*25;
     var mousePosY = 50 + (e.pageY/$(window).height())*25;
-    $('body').css('backgroundPosition', mousePosX + '% ' +  mousePosY + '%');
+    $('body').css('background-position', mousePosX + '% ' +  mousePosY + '%');
+});
+
+// Set initial zoom to be zoom out
+// TODO: make zoom in focus on mouse
+var zoomedIn = false
+$('body').click(function(e){
+    zoomedIn = !zoomedIn
+    var zoomLevel = 110 + 110 * zoomedIn
+    $('body').css('background-size', zoomLevel + '%');
 });
